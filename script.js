0ab4c08da28fa63d7f0d164b1bb7320e15af0a76
kiraGit67 <kirsten@sais.de>
2023-05-16 11:21:04 +0200

    feat: refactoring script.js

--- script.js
+++ script.js
@@ -6,12 +6,6 @@ Vue.createApp({
       inputType: "password",
       firstInput: "",
       secondInput: "",
-      buttonText: "Show Passwords",
-      passwordsAreEqual: "",
-      hasLowerCaseLetters: "",
-      hasUpperCaseLetters: "",
-      hasNumbers: "",
-      atLeast10chars: "",
     };
   },
   methods: {
@@ -20,41 +14,6 @@ Vue.createApp({
         this.inputType = "text";
       } else {
         this.inputType = "password";
-      }
-    },
-    checkEquality() {
-      if (this.passwordEquality) {
-        this.passwordsAreEqual = " ✅ ";
-      } else {
-        this.passwordsAreEqual = " ❌ ";
-      }
-    },
-    checkLowerCaseLetters() {
-      if (this.lowerCaseLetters) {
-        this.hasLowerCaseLetters = " ✅ ";
-      } else {
-        this.hasLowerCaseLetters = " ❌ ";
-      }
-    },
-    checkUpperCaseLetters() {
-      if (this.upperCaseLetters) {
-        this.hasUpperCaseLetters = " ✅ ";
-      } else {
-        this.hasUpperCaseLetters = " ❌ ";
-      }
-    },
-    checkNumbers() {
-      if (this.containsNumbers) {
-        this.hasNumbers = " ✅ ";
-      } else {
-        this.hasNumbers = " ❌ ";
-      }
-    },
-    check10orMoreChars() {
-      if (this.length10atLeast) {
-        this.atLeast10chars = " ✅ ";
-      } else {
-        this.atLeast10chars = " ❌ ";
       }
     },
   },
@@ -67,19 +26,25 @@ Vue.createApp({
       }
     },
     passwordEquality() {
-      return this.firstInput === this.secondInput;
+      return this.firstInput === this.secondInput && this.firstInput !== ""
+        ? " ✅ "
+        : " ❌ ";
     },
     lowerCaseLetters() {
-      return this.firstInput.toUpperCase() !== this.firstInput;
+      return this.firstInput.toUpperCase() !== this.firstInput
+        ? " ✅ "
+        : " ❌ ";
     },
     upperCaseLetters() {
-      return this.firstInput.toLowerCase() !== this.firstInput;
+      return this.firstInput.toLowerCase() !== this.firstInput
+        ? " ✅ "
+        : " ❌ ";
     },
     containsNumbers() {
-      return /\d/.test(this.firstInput);
+      return /\d/.test(this.firstInput) ? " ✅ " : " ❌ ";
     },
     length10atLeast() {
-      return this.firstInput.length >= 10;
+      return this.firstInput.length >= 10 ? " ✅ " : " ❌ ";
     },
   },
 }).mount("#app");
